d8765e8dcc1aa2e63068a68fb610d47d93649c53
Cameron Moeller <cameron.moeller@gmail.com>
2016-11-06 16:49:44 -0500

    returning:ids, ratings

--- movie_libb.py
+++ movie_libb.py
@@ -9,29 +9,55 @@ class Movie():
     def get_name(self, movie_id):
         return self.movie_name
 
-class Rating():
+class Rating:
 
-    def __init__(self, **kwargs):
-        self.rating = kwargs['rating']
-        self.user_id = kwargs['user_id']
+    def __init__(self, user_id, movie_id, rating,  **kwargs):
+        self.rating = rating
+        self.user_id = user_id
+        self.movie_id = movie_id
     def __repr__(self):
+        #return self.rating
         return "{}, {}".format(self.rating, self.user_id)
     def get_rating(self, movie_id):
         return self.rating
+    def get_avg(self, movie_id):
+        pass
+    def get_user_ratings(self, user_id):
+        return self.rating
 
 
-# def movie_data():
-#     with open('u.item.test.csv') as f:
-#         fieldnames = ['movie_id', 'movie_name']
-#         reader = csv.DictReader(f, fieldnames = fieldnames, delimiter = '|')
-#         dictionary_movie_ids = {}
-#         for row in reader:
-#             dictionary_movie_ids[row['movie_id']] = Movie(row['movie_name'][0:-7])
-#         print(dictionary_movie_ids)
-# movie_data()
+class User():
 
-def user_ratings():
-    pass
+    def __init__(self, **kwargs):
+        self.user_id = kwargs['user_id']
+        self.age = kwargs['age']
+        self.gender = kwargs['gender']
+        self.occupation = kwargs['occupation']
+        self.zipcode = kwargs['zipcode']
+    def __repr__(self):
+        return self.user_id
+
+
+def movie_data():
+    with open('u.item.test.csv') as f:
+        fieldnames = ['movie_id', 'movie_name']
+        reader = csv.DictReader(f, fieldnames = fieldnames, delimiter = '|')
+        dictionary_movie_ids = {}
+        for row in reader:
+            dictionary_movie_ids[row['movie_id']] = Movie(row['movie_name'][0:-7])
+        return(dictionary_movie_ids)
+movie_data()
+
+def user_data():
+    with open('u.user.test.csv') as f:
+        fieldnames = ['user_id', 'age', 'gender', 'occupation', 'zipcode']
+        reader = csv.DictReader(f, fieldnames = fieldnames, delimiter = '|')
+        dictionary_user_data = {}
+        for row in reader:
+            dictionary_user_data.setdefault(row['user_id'], []).append(User(**row))
+        #print(dictionary_user_data)
+user_data()
+
 
 def movie_ratings():
     with open('u.data.test.csv') as f:
@@ -40,8 +66,9 @@ def movie_ratings():
         dictionary_movie_ratings = {}
         for row in reader:
             dictionary_movie_ratings.setdefault(row['movie_id'], []).append(Rating(**row))
-        print(type(dictionary_movie_ratings['1'][0]))
-        print(dictionary_movie_ratings)
+        #print(type(dictionary_movie_ratings['1'][0]))
+        return(dictionary_movie_ratings)
+        #print(dictionary_movie_ratings['1'])
 movie_ratings()
 
 
--- movie_libbb.py
+++ movie_libbb.py
@@ -1,9 +1,30 @@
 from movie_libb import *
-
+import math
 
 
 def main():
-    movie_ratings()
-    print(dictionary_movie_ratings('1'))
+    menu_input = input("give me movie id: ")
+    movie_datas = movie_data()
+    print(movie_datas[menu_input])
+
+    user_input = input("give me a movie ID and i'll return the ratings: ")
+    dictratings = movie_ratings()
+    #print(dictratings.get_rating('1'))
+    print(dictratings[user_input])
+
+    user_id_input = input("give me a user id: ")
+    print(dictratings.values())
+    if user_id_input in dictratings.values():
+        print("yes")
+        print(dictratings[user_input][0].get_rating)
+    #print(.user_id)
+    #print(dictratings[user_input][0].get_rating)
+    sum_ratings = (int(dictratings[user_input][0].rating) + int(dictratings[user_input][1].rating))
+    #print(sum_ratings/len(dictratings[user_input]))
+    #print(dictratings.get_rating(user_input))
+    #print(dictratings.get_rating(user_input))
+    # print(len(dictratings[user_input]))
+    #print((sum(dictratings[user_input])))
+
 
 main()
