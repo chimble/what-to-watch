869a6407177920db021457ea4fb085b192296bb0
Cameron Moeller <cameron.moeller@gmail.com>
2016-11-06 22:07:51 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # what-to-watch
+#
+#this program will open csv files with data from a movie review site
+#based on inputs from the user, you can access information about those movies
+#run the movie_lib.py file to get started
+
+#a prompt will give you options for what information you would like to access
+#top movies will give you the highest rated movies with 300+ reviews
--- movie_lib.py
+++ movie_lib.py
@@ -1,77 +1,69 @@
-#movie class
-#u.item has
-#    movie id, movie title
-#u.data has
-#    user id, movie id, rating
-#u.user has
-#     user id, age, gender, occupation, zip
-import csv
+from movie_libb import *
+import math
 
 
-def movie_data():
-    with open('u.item.test.csv') as f:
-        fieldnames = ['movie_id', 'movie_name']
-        reader = csv.DictReader(f, fieldnames = fieldnames, delimiter = '|')
-        dict_of_movie_name_id = {}
-        for row in reader:
-            dict_of_movie_name_id[row['movie_id']] = row['movie_name'][0:-7]
-        return(dict_of_movie_name_id)
-#
-# def search_movie_by_id():
-#     dictdict = movie_data()
-#     return(dictdict[user_rating_data()])
+def main():
 
+    ranked_list = ranked_movie()
+    ask_input = input("do you want: 1:movie name by ID, 2:ratings by ID, 3: ratings by user?, 4: Top Popular Movies, 5: EXIT (1, 2, 3, 4, 5) ")
+    dictratings = movie_ratings()
+    userratings = user_ratings()
+    movie_datas = movie_data()
 
-def user_rating_data():
-    with open('u.data.test.csv') as f:
-        fieldnames = ['user_id', 'movie_id', 'rating', 'timestamp']
-        reader = csv.DictReader(f, fieldnames = fieldnames, delimiter = '\t')
-        dictdict = movie_data()
-        user_input = input("give me movie name:")
-        for movie_id, movie_name in dictdict.items():
-            if movie_name == user_input:
-                idid = movie_id
-        i=0
-        sum_rating = 0
-        for row in reader:
-            if row['movie_id'] == idid:
+    if ask_input == '4':
+        top_movie_list = []
+        i = 0
+        for item in ranked_list:
+            top_movie_list.append((movie_datas[item[0]].get_name(item[0]), ranked_list[i][1]))
+            i += 1
+        print(top_movie_list)
+        main()
+
+    elif ask_input == '1':
+        menu_input = input("give me movie id: ")
+        print(movie_datas[menu_input].get_name(menu_input))
+        main()
+
+    elif ask_input == '2':
+        decision_input = input("1: all ratings or 2: avg? (1 or 2)")
+
+        if decision_input == '1':
+            user_input = input("give me a movie ID and i'll return the ratings: ")
+            i = 0
+            list_ratings_all = []
+            for item in dictratings[user_input]:
+                list_ratings_all.append(dictratings[user_input][i])
                 i+=1
-                print(row['rating'])
-                sum_rating += int(row['rating'])
-        print(sum_rating/i)
+            print(list_ratings_all)
+            main()
+
+        else:
+            user_input = input("give me a movie ID and i'll return the avg rating: ")
+            i = 0
+            sum_ratings = 0
+            for item in dictratings[user_input]:
+                sum_ratings += (int(dictratings[user_input][i].rating))
+                i+=1
+            print(sum_ratings/len(dictratings[user_input]))
+            main()
+
+    elif ask_input == '3':
+        user_id_input = input("give me a user id: ")
+        if userratings[user_id_input]:
+            print(userratings[user_id_input])
+        main()
+
+    else:
+        exit()
+
+        #print(.user_id)
+        #print(dictratings[user_input][0].get_rating)
+
+    #print(sum_ratings/len(dictratings[user_input]))
+    #print(dictratings.get_rating(user_input))
+    #print(dictratings.get_rating(user_input))
+    # print(len(dictratings[user_input]))
+    #print((sum(dictratings[user_input])))
 
 
-user_rating_data()
-class Movie():
-    def __init__(self, movie_id, movie_name):
-        self.movie_id = movie_id
-        self.movie_name = movie_name
-
-
-
-class User():
-    def __init__(user_id, movie_id, rating):
-        self.user_id = user_id
-        self.movie_id = movie_id
-        self.rating = rating
-
-
-class Rating():
-    def __init__(self, movie_id, rating):
-        self.movie_id = movie_id
-        self.rating = rating
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#abcdefg
+main()
